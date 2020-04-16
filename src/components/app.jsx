e1ff34ccbac694e02150635a9792469fa039d0f3
Lucie Monnier <luciemonnier@MacBook-Pro-de-Lucie.local>
2020-04-16 12:39:54 +0200

    component App

---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        <div className="left-scene"></div>
+        <div className="right-scene"></div>
+    }   
+}
+
+export default App
